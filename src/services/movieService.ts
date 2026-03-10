33d75fa35ecf6144cbaee42f926dbb3afe06e3f1
Jenya Linx <sportpit2020@gmail.com>
2026-03-10 23:15:41 +0200

    fix token env

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
 interface MoviesResponse {
   results: Movie[];
 }
@@ -13,7 +15,7 @@ export const fetchMovies = async (query: string): Promise<Movie[]> => {
         query: query,
       },
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODRlZWRkNDhiZjFkZDM0ZTU2NjE3N2E0OWJjMWM0NSIsIm5iZiI6MTc3MzE0ODQwOS42NjEsInN1YiI6IjY5YjAxOGY5YWZiYjkzOTgwM2MxZjU2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1xBkwDkFU5GiArqiBdeCSFqlR1hzYAa2mc_YnMEoNRM`,
+        Authorization: `Bearer ${TOKEN}`,
       },
     }
   );
